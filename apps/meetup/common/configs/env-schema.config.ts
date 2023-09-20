abbb2d0d5640deeefce522d43d1555e94523ff75
Schanya <anyashemelevadm@gmail.com>
2023-09-20 16:36:40 +0300

    edit prisma and env-validation schema

---
+++ apps/meetup/common/configs/env-schema.config.ts
@@ -0,0 +1,8 @@
+import { ConfigModuleOptions } from '@nestjs/config';
+import { envValidationSchema } from '../schemas/env-validation.schema';
+
+export const envSchemaOptions: ConfigModuleOptions = {
+  isGlobal: true,
+  validationSchema: envValidationSchema,
+  envFilePath: './apps/meetup/.env',
+};
--- apps/meetup/prisma/seeds/seed.ts
+++ apps/meetup/prisma/seeds/seed.ts
@@ -5,21 +5,82 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.$connect();
 
-  await prisma.tags.create({
+  // await prisma.tags.create({
+  //   data: {
+  //     title: 'tag 1',
+  //   },
+  // });
+
+  // await prisma.tags.create({
+  //   data: {
+  //     title: 'tag 2',
+  //   },
+  // });
+
+  // await prisma.tags.create({
+  //   data: {
+  //     title: 'tag 3',
+  //   },
+  // });
+
+  await prisma.meetups.create({
     data: {
-      title: 'tag 1',
+      title: 'meetup 1',
+      description: 'description to meetup 1',
+      place: 'Vitebsk, Bogdan Khmelnitsky Street, 30',
+      date: '12-12-2022 12:12:12',
+      // organizerId: 1,
+      tags: {
+        create: [
+          {
+            tag: { create: { title: 'dev' } },
+          },
+          {
+            tag: { create: { title: 'js' } },
+          },
+        ],
+      },
     },
   });
 
-  await prisma.tags.create({
+  await prisma.meetups.create({
     data: {
-      title: 'tag 2',
+      title: 'meetup 2',
+      description: 'description to meetup 2',
+      place: 'Vitebsk, Bogdan Khmelnitsky Street, 30',
+      date: '12-12-2022 12:12:12',
+      // organizerId: 2,
+      tags: {
+        create: [
+          {
+            tag: { create: { title: 'nest' } },
+          },
+          {
+            tag: { create: { title: 'ts' } },
+          },
+        ],
+      },
     },
   });
 
-  await prisma.tags.create({
+  await prisma.meetups.create({
     data: {
-      title: 'tag 3',
+      title: 'meetup 3',
+      description: 'description to meetup 3',
+      place: 'Vitebsk, Bogdan Khmelnitsky Street, 30',
+      date: '12-12-2022 12:12:12',
+      // organizerId: 2,
+      tags: {
+        create: [
+          {
+            tag: { create: { title: 'database' } },
+          },
+          {
+            tag: { create: { title: 'postgresql' } },
+          },
+        ],
+      },
+      // members: { create: { userId: 1 } },
     },
   });
 }
