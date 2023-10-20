bc53ebf60f60c6c5d02e6253ed47334ebd70e9a1
Schanya <anyashemelevadm@gmail.com>
2023-10-20 08:31:16 +0300

    add .example.env, rename .envDocker

--- apps/auth/common/schemas/env-validation.schema.ts
+++ apps/auth/common/schemas/env-validation.schema.ts
@@ -1,5 +1,13 @@
 import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
+  DATABASE_URL: Joi.string().required(),
   RABBIT_MQ_URI: Joi.string().required(),
+  RABBIT_MQ_AUTH_QUEUE: Joi.string().required(),
+  ACCESS_TOKEN_SECRET: Joi.string().required(),
+  ACCESS_TOKEN_EXPIRED: Joi.string().required(),
+  REFRESH_TOKEN_SECRET: Joi.string().required(),
+  REFRESH_TOKEN_EXPIRED: Joi.string().required(),
+  GOOGLE_CLIENT_ID: Joi.string().required(),
+  GOOGLE_SECRET: Joi.string().required(),
 });
--- apps/gateway/src/common/schemas/env-validation.schema.ts
+++ apps/gateway/src/common/schemas/env-validation.schema.ts
@@ -2,4 +2,15 @@ import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
   RABBIT_MQ_URI: Joi.string().required(),
+  RABBIT_MQ_AUTH_QUEUE: Joi.string().required(),
+  RABBIT_MQ_MEETUP_QUEUE: Joi.string().required(),
+  ACCESS_TOKEN_SECRET: Joi.string().required(),
+  REFRESH_TOKEN_SECRET: Joi.string().required(),
+  GOOGLE_CLIENT_ID: Joi.string().required(),
+  GOOGLE_SECRET: Joi.string().required(),
+  GOOGLE_REDIRECT_URL: Joi.string().required(),
+  AWS_ACCESS_KEY_ID: Joi.string().required(),
+  AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+  AWS_BUCKET_NAME: Joi.string().required(),
+  AWS_ROUTE_NAME: Joi.string().required(),
 });
